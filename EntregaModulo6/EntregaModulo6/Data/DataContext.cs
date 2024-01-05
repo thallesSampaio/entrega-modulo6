ea25ec2a597234a5e4ab636e2a54df4d48a26d06
thallesSampaio <thallessampaio89@gmail.com>
2024-01-05 20:30:06 -0300

    first commit

---
+++ EntregaModulo6/EntregaModulo6/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using EntregaModulo6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntregaModulo6.Data
+{
+    public class DataContext  : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public DbSet<Mensagem> Mensage { get; set; }
+
+    }
+}
