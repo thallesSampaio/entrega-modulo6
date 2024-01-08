382da5c5341efd3d6155d8d80a56da68708a61ce
thallesSampaio <thallessampaio89@gmail.com>
2024-01-08 19:41:34 -0300

    added controller

---
+++ EntregaModulo6/EntregaModulo6/Controllers/PacoteController.cs
@@ -0,0 +1,39 @@
+﻿using EntregaModulo6.Data;
+using EntregaModulo6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntregaModulo6.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacoteController : ControllerBase
+    {
+
+        private readonly DataContext _dataContext;
+
+        public PacoteController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pacote>>> GetPacote()
+        {
+            return await _dataContext.Pacote.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pacote>> GetPacoteById(int id)
+        {
+            var pacote = await _dataContext.Pacote.FindAsync(id);
+            if (pacote == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pacote);
+        }
+    }
+}
